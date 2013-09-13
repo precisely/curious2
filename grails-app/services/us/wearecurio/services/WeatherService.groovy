e2937ed36a8427b10689a5fa51887ba52ce5954c
visheshd <vishesh@causecode.com>
2013-09-13 12:10:08 +0530

    Getting weather conditions for a date in the past #80

--- grails-app/services/us/wearecurio/services/WeatherService.groovy
+++ grails-app/services/us/wearecurio/services/WeatherService.groovy
@@ -1,4 +1,6 @@
 package us.wearecurio.services
+import java.text.SimpleDateFormat;
+
 import grails.converters.JSON;
 import us.wearecurio.http.*
 
@@ -6,12 +8,27 @@ class WeatherService {
 	def grailsApplication
 	
 	
-	def fetchConditions(def city="San_Francisco", def state ="CA", def date = new Date()) {
+	def fetchConditions(def city="San_Francisco", def state ="CA") {
 		def key = grailsApplication.config.api.weatherunderground.key
 		def url = "http://api.wunderground.com/api/${key}/conditions/q/${state}/${city}.json"
 		HttpConnection connection = new HttpConnection(url)
 		def result = connection.sendGet()
 		result = JSON.parse(result)
-		return result.current_observation
+		return result
+	}
+	
+	def fetchHistory(def city="San_Francisco", def state ="CA", def date = new Date()) {
+		def key = grailsApplication.config.api.weatherunderground.key
+		SimpleDateFormat format = new SimpleDateFormat("yyyyMMdd")
+		def url = "http://api.wunderground.com/api/${key}/history_${format.format(date)}/q/${state}/${city}.json"
+		
+		if (date >= new Date().clearTime()) {
+		//	return fetchConditions(city,state)
+		}
+		
+		HttpConnection connection = new HttpConnection(url)
+		def result = connection.sendGet()
+		result = JSON.parse(result)
+		return result
 	}
 }
