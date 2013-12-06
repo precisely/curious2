ba5ee81863813ee0684f28ec14afd9c653595f3e
Shashank Agrawal <shashank.agrawal@causecode.com>
2013-12-06 11:10:48 +0530

    Fixed timejob expression. #127

--- grails-app/utils/us/wearecurio/utility/TimerJob.groovy
+++ grails-app/utils/us/wearecurio/utility/TimerJob.groovy
@@ -5,6 +5,6 @@ class TimerJob {
 	public static final long SECOND = 1000L;
 	public static final long MINUTE = 60 * SECOND
 	public static final long HOUR = 60 * MINUTE
-	public static final long DAY = 24 * 60 * HOUR
+	public static final long DAY = 24 * HOUR
 
 }
