1d1106137d114a27c979f9d27db2a5270bf51e2e
Vishesh Duggar <vishesh@causecode.com>
2014-01-10 16:25:50 +0530

    Converting mobile/index.gsp to plain html file. For easier packaging as mobile app

--- grails-app/controllers/us/wearecurio/controller/MobileController.groovy
+++ grails-app/controllers/us/wearecurio/controller/MobileController.groovy
@@ -14,35 +14,4 @@ class MobileController extends SessionController {
 
 	def MobileController() {
 	}
-
-	def cachemanifest() {
-		if(Environment.current == Environment.DEVELOPMENT) {
-			response.sendError(500)	// Skipping cache on development.
-		}
-		debug "MobileController.cachemanifest()"
-		render(contentType:"text/cache-manifest", view:"/mobile/cachemanifest", model:[templateVer:urlService.template(request)])
-	}
-
-	def index() {
-		debug "MobileController.index()"
-
-		def user = sessionUser()
-		if (user != null) {
-			debug "auth user:" + user
-			def entryId = -1
-			if (params.entryId) {
-				entryId = Long.parseLong(params.entryId)
-			}
-			[prefs:user.getPreferences(), login:0, templateVer:urlService.template(request), entryId:entryId]
-		} else {
-			debug "auth failure"
-			[prefs:null, login:0, templateVer:urlService.template(request), entryId:-1]
-		}
-	}
-
-	def login() {
-		debug "MobileController.login()"
-
-		render(view: "/mobile/index", model: [login:1,templateVer:urlService.template(request)])
-	}
 }
