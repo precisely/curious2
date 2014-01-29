92b4318f78ba3cac9e064da04bd0e9a506284f1c
Shashank Agrawal <shashank.agrawal@causecode.com>
2014-01-29 18:10:17 +0530

    Fix one migration which may fail if migration already exist. #146

--- test/integration/MigrationServiceTests.groovy
+++ test/integration/MigrationServiceTests.groovy
@@ -1,19 +1,17 @@
-import grails.test.*
-
-import us.wearecurio.services.MigrationService
-import us.wearecurio.server.Session
-import us.wearecurio.model.User
-import us.wearecurio.utility.Utils
-
 import static org.junit.Assert.*
-import org.junit.*
+import grails.test.*
 import grails.test.mixin.*
+
+import org.junit.*
+
+import us.wearecurio.server.Migration
+import us.wearecurio.services.MigrationService
 
 class MigrationServiceTests extends GroovyTestCase {
 	static transactional = true
 
 	def migrationService
-	
+
 	@Before
 	void setUp() {
 	}
@@ -25,13 +23,16 @@ class MigrationServiceTests extends GroovyTestCase {
 	@Test
 	void testDoMigration() {
 		def didMigration = false
-		
+
+		// Confirm no existance of test migration id.
+		Migration.findByCode(MigrationService.TEST_MIGRATION_ID)?.delete()
+
 		migrationService.tryMigration(MigrationService.TEST_MIGRATION_ID) {
 			didMigration = true
 		}
-		
+
 		assertTrue(didMigration)
-		
+
 		assert migrationService.shouldDoMigration(MigrationService.TEST_MIGRATION_ID) == null
 	}
 }
