f312288727907d91da62f36bdd9fcbe12e24cf03
Shashank Agrawal <shashank.agrawal@causecode.com>
2014-02-03 13:23:02 +0530

    Using common service to create/update OAuthAccount after authentication. #163

--- grails-app/controllers/us/wearecurio/controller/AuthenticationController.groovy
+++ grails-app/controllers/us/wearecurio/controller/AuthenticationController.groovy
@@ -7,7 +7,6 @@ import org.codehaus.groovy.grails.web.json.JSONObject
 import org.scribe.model.Token
 
 import us.wearecurio.model.OAuthAccount
-import us.wearecurio.model.User
 
 /**
  * A generic controller to handle all authentication made by oauth plugin.
@@ -69,61 +68,37 @@ class AuthenticationController extends SessionController {
 		if (request.queryString) {
 			returnURI += "?" + request.queryString
 		}
+
 		session.returnURIWithToken = returnURI.substring(1)	// Removing "/" from beginning since serverURL is configured with "/" at last.
-		redirect(url: toUrl(action: "authenticate", controller: "oauth", params: [provider: provider]))	// redirecting to oauth plugin controller
+		// Redirecting to oauth plugin controller
+		redirect(url: toUrl(action: "authenticate", controller: "oauth", params: [provider: provider]))
 		return
 	}
 
 	def twenty3andmeAuth() {
-		User currentUserInstance = sessionUser()
+		JSONObject userInfo = twenty3AndMeDataService.getUserProfiles(tokenInstance)
 
-		// This will always update access_token to latest access_token.
-		JSONObject parsedResponse = twenty3AndMeDataService.getUserProfiles(tokenInstance)
-
-		if (parsedResponse.id) {
-			OAuthAccount.createOrUpdate(OAuthAccount.TWENTY_3_AND_ME_ID, currentUserInstance.id, parsedResponse.id,
-					tokenInstance.token, tokenInstance.secret ?: "")
-		}
+		oAuthAccountService.createOrUpdate(TWENTY_3_AND_ME_ID, userInfo.id, tokenInstance)
 	}
 
 	def fitbitAuth() {
-		User currentUserInstance = sessionUser()
-
-		// This will always update access_token to latest access_token.
 		// Since FitBit doesn't return user info in response to an authentication we explicitly ask for it
 		JSONObject userInfo =  fitBitDataService.getUserInfo(tokenInstance)
 
-		if (userInfo.user) {
-			OAuthAccount.createOrUpdate(OAuthAccount.FITBIT_ID, currentUserInstance.id,
-					userInfo.user.encodedId, tokenInstance.token, tokenInstance.secret ?: "")
-		}
+		oAuthAccountService.createOrUpdate(FITBIT_ID, userInfo.user.encodedId, tokenInstance)
 	}
 
 	def humanAuth() {
-		User currentUserInstance = sessionUser()
 		JSONObject userInfo = humanDataService.getUserProfile(tokenInstance)
 
-		if(userInfo.userId) {
-			OAuthAccount.createOrUpdate(HUMAN_ID, currentUserInstance.id, userInfo.userId, tokenInstance.token, "")
-		} else {
-			log.error "userId not found in human api authentication."
-		}
+		oAuthAccountService.createOrUpdate(HUMAN_ID,  userInfo.userId, tokenInstance.token, "")
 	}
 
 	def movesAuth() {
-		User currentUserInstance = sessionUser()
+		// Moves sends user_id while getting the access token.
+		JSONObject userInfo = JSON.parse(tokenInstance.rawResponse)
 
-		String rawResponse = tokenInstance.rawResponse
-		if (rawResponse) {	// Moves sends user_id while getting the access token.
-			JSONObject parsedResponse = JSON.parse(rawResponse)
-
-			if (parsedResponse.user_id) {
-				OAuthAccount.createOrUpdate(OAuthAccount.MOVES_ID, currentUserInstance.id,
-						parsedResponse.user_id.toString(), tokenInstance.token, tokenInstance.secret ?: "")
-			}
-		} else {
-			log.warn "Unable to create or update oauth account for moves. No raw response found in token."
-		}
+		oAuthAccountService.createOrUpdate(MOVES_ID, userInfo.user_id.toString(), tokenInstance)
 	}
 
 	def withingsAuth() {
--- grails-app/services/us/wearecurio/services/OAuthAccountService.groovy
+++ grails-app/services/us/wearecurio/services/OAuthAccountService.groovy
@@ -1,11 +1,31 @@
 package us.wearecurio.services
 
+import org.scribe.model.Token
+
 import us.wearecurio.model.OAuthAccount
 import us.wearecurio.model.User
 
 class OAuthAccountService {
 
 	def securityService
+
+	/**
+	 * Used to create OAuthAccount instance for given provider/type for current loggedIn user.
+	 * @param typeId Type of the provider. See constants in OAuthAccount.groovy.
+	 * @param accountId Account Identity for current provider.
+	 * @param accessToken Access Token after authorization.
+	 * @param accessSeret Access Secret after authorization.
+	 * @return OAuthAccount instance for given parameters.
+	 */
+	OAuthAccount createOrUpdate(int typeId, String accountId, String accessToken, String accessSeret) {
+		User currentUser = securityService.currentUser
+
+		OAuthAccount.createOrUpdate(typeId, currentUser.id, accountId, accessToken, accessSeret ?: "")
+	}
+
+	OAuthAccount createOrUpdate(int typeId, String accountId, Token tokenInstance) {
+		createOrUpdate(typeId, accountId, tokenInstance.token, tokenInstance.secret)
+	}
 
 	boolean isLinked(int typeId) {
 		User currentUser = securityService.currentUser
