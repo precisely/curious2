0b5eead2ace43530a9d1f0c38ae0325d3f7aae90
Vishesh Duggar <vishesh@causecode.com>
2014-02-27 20:12:16 +0530

    Fix to #200 prevents refreshing cache for a regular scroll up.

--- web-app/js/mobile/mobileBase.js
+++ web-app/js/mobile/mobileBase.js
@@ -560,7 +560,8 @@ $(document).ready(function() {
 		console.log('Move Start Y on move: ' + window.moveStartY);
 		console.log('pageY on move: ' + event.pageY);
 		console.log('Move Direction: ' + moveVerticalDirection);
-		if (moveVerticalDirection < 0 && -moveVerticalDirection > 40) {
+		console.log('Window scroll position:' + $(body).scroo)
+		if (moveVerticalDirection < 0 && -moveVerticalDirection > 40 && $('#recordList').scrollTop() < 0) {
 			$('#fetchingData').show();
 			fetchEntries(cachedDateUTC, function (entries) {
 				refreshEntries(entries, true);
