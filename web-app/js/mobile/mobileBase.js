03a1633dd3d0d79331afc39d96c5674ee9e148f9
Mitsu Hadeishi <mitsu@syntheticzero.com>
2014-03-02 22:06:07 -0500

    Fixes #211 Prepending "appCache." to keys for all app cache entries, to avoid namespace collisions. Clearing localStorage in its entirety on logout, also clearing entries from interface.

--- web-app/js/mobile/mobileBase.js
+++ web-app/js/mobile/mobileBase.js
@@ -32,14 +32,19 @@ function showYesNo(alertText, onConfirm) {
 	});
 }
 
+var localStorageIsSupported = 2;
+
 var localStorageSupported = function() {
+	if (localStorageIsSupported == true) return true;
+	if (localStorageSupported == false) return false;
 	try {
-		localStorage.setItem("test", "test");
-		localStorage.removeItem("test");
-		return 'localStorage' in window && window['localStorage'] !== null;
+		localStorage.setItem("_test__", "_test__");
+		localStorage.removeItem("_test__");
+		localStorageSupported = ('localStorage' in window && window['localStorage'] !== null);
 	} catch (e) {
-		return false;
+		localStorageSupported = false;
 	}
+	return localStorageSupported;
 }
 
 if (!localStorageSupported()) {
@@ -70,9 +75,11 @@ var activateEntryId = -1;
 
 function doLogout() {
 	console.log("Logging out...");
+	clearEntries();
 	callLogoutCallbacks();
+	currentUserId = undefined;
+	localStorage.clear();
 	localStorage['mobileSessionId'] = null;
-	localStorage['appCache'] = null;
 	localStorage['lastPage'] = 'login';
 	startLogin(0);
 	$(document).trigger("ask-logout");
@@ -250,40 +257,44 @@ function makePlainUrl(url) {
 	return url;
 }
 
-function initAppCache() {
+function clearAppCache() {
 	if (supportsLocalStorage()) {
-		if (localStorage['appCache'] == null) {
-			localStorage['appCache'] = {};
-			localStorage['lastPage'] = null;
-			return;
+		for (var key in localStorage) {
+			if (key.indexOf("appCache.") == 0) {
+				localStorage.removeItem(key);
+			}
 		}
 	}
+	
+	localStorage['lastPage'] = null;
 }
 
 function getAppCacheData(key) {
+	var aKey = 'appCache.' + key;
 	if (supportsLocalStorage()) {
 		try {
-			return JSON.parse(localStorage[key]);
+			return JSON.parse(localStorage[aKey]);
 		} catch(err) {
-			console.log('Unable to fetch or parse data from cache. Key: '+key);
-			console.log('Cache value at '+key+': '+localStorage[key]);
+			console.log('Unable to fetch or parse data from cache. Key: '+aKey);
+			console.log('Cache value at ' + aKey + ': ' + localStorage[aKey]);
 			return null;
 		}
 	}
 }
 
 function setAppCacheData(key,value) {
+	var aKey = 'appCache.' + key;
 	if (supportsLocalStorage()) {
 		try {
 			if (typeof value == 'object') {
-				localStorage[key] = JSON.stringify(value);
+				localStorage[aKey] = JSON.stringify(value);
 			} else {
-				localStorage[key] = value;
+				localStorage[aKey] = value;
 			}
 			
 			return true;
 		} catch(err) {
-			console.log('Unable to save data to cache. Error: '+err);
+			console.log('Unable to save data to cache. Error: ' + err);
 			return false;
 		}
 	}
@@ -303,7 +314,7 @@ function getEntryBucket() {
 		// for installations that already have cached data
 		// but no buckets
 		for (var prop in localStorage) {
-			if (prop.indexOf('appCache.entryCache.') > -1) {
+			if (prop.indexOf('appCache.entryCache.') == 0) {
 				//Pushing just the date part
 				entryBucket.push(prop.substring(20));
 			}
@@ -343,7 +354,7 @@ function getEntryCache(date) {
 	var month = ("0" + (date.getMonth() + 1)).slice(-2);
 	var day = ("0" + date.getDate()).slice(-2);
 	var dateStr = month + '/' + day + '/' + (date.getYear() + 1900);
-	return getAppCacheData('appCache.entryCache.'+dateStr);
+	return getAppCacheData('appCache.entryCache.' + dateStr);
 	
 }
 
@@ -367,11 +378,11 @@ function setEntryCache(date,entries) {
 		dateStr = date;
 	}
 	
-	if (setAppCacheData('appCache.entryCache.'+dateStr,entries)) {
+	if (setAppCacheData('entryCache.'+dateStr, entries)) {
 		if (!isEntryCached(dateStr)) {
 			entryBucket.push(dateStr);
 			if (entryBucket.length > 10) {
-				localStorage.removeItem('appCache.entryCache.'+entryBucket[0]);
+				localStorage.removeItem('appCache.entryCache.' + entryBucket[0]);
 				entryBucket.shift();
 			}
 			setEntryBucket(entryBucket);
@@ -384,7 +395,7 @@ function setEntryCache(date,entries) {
 
 function clearEntryCache() {
 	for (var key in localStorage) {
-		if (key.indexOf("appCache.entryCache")>-1) {
+		if (key.indexOf("appCache.entryCache") == 0) {
 			localStorage.removeItem(key);
 		}
 	}
@@ -404,8 +415,8 @@ function startLogin(mode) {
 		loginMode = mode;
 
 		if (supportsLocalStorage()) {
+			localStorage.clear();
 			localStorage['mobileSessionId'] = null;
-			clearEntryCache();
 			localStorage['lastPage'] = 'login';
 		}
 		$('#trackPage').hide();
