95383189e64098e375ad68977d4b28e17286ce3b
Mitsu Hadeishi <mitsu@syntheticzero.com>
2014-05-07 07:01:04 -0400

    Re #318 add environment to email

--- grails-app/utils/us/wearecurio/handlers/CuriousExceptionResolver.groovy
+++ grails-app/utils/us/wearecurio/handlers/CuriousExceptionResolver.groovy
@@ -9,17 +9,22 @@ import org.codehaus.groovy.grails.web.errors.GrailsExceptionResolver
 import org.codehaus.groovy.runtime.InvokerInvocationException
 import org.springframework.web.servlet.ModelAndView
 
+import grails.util.GrailsUtil
+
 class CuriousExceptionResolver extends GrailsExceptionResolver {
 	private static def log = LogFactory.getLog(this)
 	
 	@Override
 	public ModelAndView resolveException(HttpServletRequest request, HttpServletResponse response, Object handler, Exception e) {
+		if (GrailsUtil.environment.equals('development'))
+			return super.resolveException(request, response, handler, e)
+		
 		Throwable t = e
 		
 		while (t != null) {
 			if (t instanceof us.wearecurio.thirdparty.AuthenticationRequiredException)
 				// don't notify on this exception, should be handled by Grails
-				return super.resolveException(request, response, handler, e);
+				return super.resolveException(request, response, handler, e)
 			t = t.getCause()
 		}
 		
@@ -31,7 +36,7 @@ class CuriousExceptionResolver extends GrailsExceptionResolver {
 		log.error(output)
 	   
 	   	def messageBody = "Error while executing Curious app:\n" + output
-		def messageSubject = "CURIOUS SERVER ERROR"
+		def messageSubject = "CURIOUS SERVER ERROR: " + GrailsUtil.environment
 		Utils.getMailService().sendMail {
 			to "server@wearecurio.us"
 			from "server@wearecurio.us"
@@ -39,6 +44,6 @@ class CuriousExceptionResolver extends GrailsExceptionResolver {
 			body messageBody
 		}
 		
-		return super.resolveException(request, response, handler, e);
+		return super.resolveException(request, response, handler, e)
 	}
 }
