0b5bddb5474edaa56f2e5e6901b20a66dde59397
Vishesh Duggar <vishesh@causecode.com>
2014-05-13 22:56:47 +0530

    Partial commit #295

--- grails-app/controllers/us/wearecurio/controller/DiscussionController.groovy
+++ grails-app/controllers/us/wearecurio/controller/DiscussionController.groovy
@@ -3,101 +3,101 @@ package us.wearecurio.controller
 import org.springframework.dao.DataIntegrityViolationException
 import us.wearecurio.model.*
 
-class DiscussionController {
+class DiscussionController extends LoginController {
 
-    static allowedMethods = [save: "POST", update: "POST", delete: "POST"]
+	static allowedMethods = [save: "POST", update: "POST", delete: "POST"]
 
-    def index() {
-        redirect(action: "list", params: params)
-    }
 
-    def list(Integer max) {
-        params.max = Math.min(max ?: 10, 100)
-        [discussionInstanceList: Discussion.list(params), discussionInstanceTotal: Discussion.count()]
-    }
+	private def loadGroup(def params) {
+		debug "DiscussionController.loadGroup()"
+		def user = sessionUser()
+		def p = params
 
-    def create() {
-        [discussionInstance: new Discussion(params)]
-    }
+		UserGroup group = p.group ? UserGroup.lookup(p.group) : UserGroup.getDefaultGroupForUser(user)
+		if (group && !group.hasWriter(user)) {
+			return false
+		}
+		return group
+	}
 
-    def save() {
-        def discussionInstance = new Discussion(params)
-        if (!discussionInstance.save(flush: true)) {
-            render(view: "create", model: [discussionInstance: discussionInstance])
-            return
-        }
+	private def loadDiscussion(def params) {
+		def discussion
+		if (params.id) {
+			discussion = Discussion.get(params.id)
+		} else if (params.plotDataId) {
+			discussion = Discussion.getDiscussionForPlotDataId(params.plotDataId)
+			debug "Discussion for plotDataId not found: " + params.plotDataId
+		}
+		return discussion
+	}
 
-        flash.message = message(code: 'default.created.message', args: [message(code: 'discussion.label', default: 'Discussion'), discussionInstance.id])
-        redirect(action: "show", id: discussionInstance.id)
-    }
+	def index() {
+		redirect(action: "list", params: params)
+	}
 
-    def show(Long id) {
-        def discussionInstance = Discussion.get(id)
-        if (!discussionInstance) {
-            flash.message = message(code: 'default.not.found.message', args: [message(code: 'discussion.label', default: 'Discussion'), id])
-            redirect(action: "list")
-            return
-        }
+	def list(Integer max) {
+		params.max = Math.min(max ?: 10, 100)
+		[discussionInstanceList: Discussion.list(params), discussionInstanceTotal: Discussion.count()]
+	}
 
-        [discussionInstance: discussionInstance]
-    }
+	def create() {
+		def user = sessionUser()
+		def group = loadGroup(params)
+		debug "DiscussionController.create to group: " + group?.dump()
+		if (!group) {
+			flash.message = "Failed to create new discussion topic: can't post to this group"
+		} else {
+			Discussion discussion = Discussion.create(user, p.name)
+			if (discussion != null)
+				Utils.save(discussion, true)
+				flash.message = "Created new discussion: " + params.name
+				if (group) group.addDiscussion(discussion)
+			else {
+				debug "DiscussionId not found: " + discussionId
+				flash.message = "Failed to create new discussion topic: internal error"
+			}
+		}
 
-    def edit(Long id) {
-        def discussionInstance = Discussion.get(id)
-        if (!discussionInstance) {
-            flash.message = message(code: 'default.not.found.message', args: [message(code: 'discussion.label', default: 'Discussion'), id])
-            redirect(action: "list")
-            return
-        }
+		redirect(url:toUrl(controller:'home', action:'community'))
+		return
+	}
 
-        [discussionInstance: discussionInstance]
-    }
 
-    def update(Long id, Long version) {
-        def discussionInstance = Discussion.get(id)
-        if (!discussionInstance) {
-            flash.message = message(code: 'default.not.found.message', args: [message(code: 'discussion.label', default: 'Discussion'), id])
-            redirect(action: "list")
-            return
-        }
+	def show(Long id) {
+		def discussionInstance = Discussion.get(id)
+		if (!discussionInstance) {
+			flash.message = message(code: 'default.not.found.message', args: [message(code: 'discussion.label', default: 'Discussion'), id])
+			redirect(action: "list")
+			return
+		}
 
-        if (version != null) {
-            if (discussionInstance.version > version) {
-                discussionInstance.errors.rejectValue("version", "default.optimistic.locking.failure",
-                          [message(code: 'discussion.label', default: 'Discussion')] as Object[],
-                          "Another user has updated this Discussion while you were editing")
-                render(view: "edit", model: [discussionInstance: discussionInstance])
-                return
-            }
-        }
+		[discussionInstance: discussionInstance]
+	}
 
-        discussionInstance.properties = params
+	def updateData(Long id) {
+		def discussionInstance = Discussion.get(id)
+		// TODO update a discussion
+		if (p.id) {
+			discussion = Discussion.get(p.id)
+			discussion ?: (message = "That discussion topic no longer exists.")
+		} else if (plotDataId) {
+			discussion = Discussion.getDiscussionForPlotDataId(plotDataId)
+			debug "Discussion for plotDataId not found: " + plotDataId
+			discussion ?: (message = "That shared graph discussion no longer exists.")
+		}
+	}
 
-        if (!discussionInstance.save(flush: true)) {
-            render(view: "edit", model: [discussionInstance: discussionInstance])
-            return
-        }
+	def deleteData(Long id) {
+		def discussionInstance = Discussion.get(id)
 
-        flash.message = message(code: 'default.updated.message', args: [message(code: 'discussion.label', default: 'Discussion'), discussionInstance.id])
-        redirect(action: "show", id: discussionInstance.id)
-    }
-
-    def delete(Long id) {
-        def discussionInstance = Discussion.get(id)
-        if (!discussionInstance) {
-            flash.message = message(code: 'default.not.found.message', args: [message(code: 'discussion.label', default: 'Discussion'), id])
-            redirect(action: "list")
-            return
-        }
-
-        try {
-            discussionInstance.delete(flush: true)
-            flash.message = message(code: 'default.deleted.message', args: [message(code: 'discussion.label', default: 'Discussion'), id])
-            redirect(action: "list")
-        }
-        catch (DataIntegrityViolationException e) {
-            flash.message = message(code: 'default.not.deleted.message', args: [message(code: 'discussion.label', default: 'Discussion'), id])
-            redirect(action: "show", id: id)
-        }
-    }
+		try {
+			discussionInstance.delete(flush: true)
+			renderJSONPost(['success', [message: message]])
+			return
+		}
+		catch (DataIntegrityViolationException e) {
+			renderJSONPost(['error', [message: message]])
+			return
+		}
+	}
 }
