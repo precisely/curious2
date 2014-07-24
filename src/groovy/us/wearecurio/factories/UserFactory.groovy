aee2410c977b82cdf55d06e135cfcbe5f15c9c80
Shashank Agrawal <shashank.agrawal@causecode.com>
2014-07-24 17:29:28 +0530

    Just code formatted.

--- src/groovy/us/wearecurio/factories/UserFactory.groovy
+++ src/groovy/us/wearecurio/factories/UserFactory.groovy
@@ -1,19 +1,21 @@
 package us.wearecurio.factories
+
 import us.wearecurio.model.User
 
 class UserFactory {
-    public static def generate_email(username) {
-      "${username}@${username}.com"
-    }
 
-    public static def make(params=[:]) {
-    params['username'] = params['username'] ?: 'y'
-    params['email'] = params['email'] ?: generate_email(params['username'])
-    params['sex'] = params['sex'] ?: 'F'
-    params['birthdate'] = params['birthdate'] ?: '01/01/2001'
-    params['first'] = params['first'] ?: params['username']
-    params['last'] = params['last'] ?: params['username']
-    params['password'] = params['password'] ?: params['username']
+	static String generate_email(username) {
+		"${username}@${username}.com"
+	}
+
+	static User make(Map params = [:]) {
+		params['username'] = params['username'] ?: 'y'
+		params['email'] = params['email'] ?: generate_email(params['username'])
+		params['sex'] = params['sex'] ?: 'F'
+		params['birthdate'] = params['birthdate'] ?: '01/01/2001'
+		params['first'] = params['first'] ?: params['username']
+		params['last'] = params['last'] ?: params['username']
+		params['password'] = params['password'] ?: params['username']
 
 		def user = User.findByUsername(params['username'])
 
@@ -21,13 +23,13 @@ class UserFactory {
 			user = User.create(params)
 		}
 
-    for (p in params) {
-      if (p.key == 'birthdate') {
-        p.value = new Date(p.value)
-      }
-      user.setProperty(p.key, p.value)
-    }
+		for (p in params) {
+			if (p.key == 'birthdate') {
+				p.value = new Date(p.value)
+			}
+			user.setProperty(p.key, p.value)
+		}
 		user.save()
 		return user
-  }
+	}
 }
