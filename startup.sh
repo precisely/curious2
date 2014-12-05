7e6b215e456bc2def49aae3b270c29d6596c8a86
ujjawalgayakwad <ujjawal.gayakwad@causecode.com>
2014-12-05 12:01:32 +0530

    Updated readme file and set JAVA_HOME environment variable in startup.sh file. #456

--- startup.sh
+++ startup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 service mysql start
+export JAVA_HOME=/usr/java/jdk1.7.0_51
 export GRAILS_HOME="/vagrant/current-grails"
-export PATH="{$PATH}:$GRAILS_HOME/bin"
+export PATH="$PATH:$GRAILS_HOME/bin"
