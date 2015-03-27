51a87f6c7a6710714034235b6f5756e95bc93455
ujjawalgayakwad <ujjawal.gayakwad@causecode.com>
2015-03-27 16:24:22 +0530

    Fixed removing tags from and deleting TagGroup bug. #528

---
+++ test/integration/us/wearecurio/controller/integration/TagControllerTests.groovy
@@ -0,0 +1,76 @@
+package us.wearecurio.controller.integration
+
+import java.text.DateFormat
+import java.util.Date
+
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+
+import us.wearecurio.controller.TagController
+import grails.test.*
+import us.wearecurio.model.*
+import us.wearecurio.utility.Utils
+
+class TagControllerTests extends CuriousControllerTestCase{
+
+	def tagGroupService
+
+	TagController controller
+	DateFormat dateFormat
+	Date earlyBaseDate
+	Date currentTime
+	Date endTime
+	String timeZone // simulated server time zone
+	Date baseDate
+	Tag tag1, tag2
+	TagGroup tagGroupInstance
+
+	@Before
+	void setUp() {
+		super.setUp()
+
+		controller = new TagController()
+
+		tag1 = Tag.create("bread")
+		tag2 = Tag.create("apple")
+		tagGroupInstance = tagGroupService.createTagGroup("Demo tag group", userId, null)
+		tagGroupService.addTags(tagGroupInstance, "$tag1.id, $tag2.id")
+	}
+
+	@After
+	void tearDown() {
+		super.tearDown()
+	}
+
+	@Test
+	void testRemoveTagFromTagGroupData() {
+		
+		assert tagGroupInstance.getTags(userId).size() == 2
+		
+		controller.session.userId = userId
+		controller.params["tagGroupId"] = tagGroupInstance.id
+		controller.params["id"] = tag1.id
+		
+		controller.removeTagFromTagGroupData()
+		
+		assert controller.response.json.success == true
+		assert tagGroupInstance.getTags(userId).size() == 1
+		assert !tagGroupInstance.containsTag(tag1, userId)
+	}
+
+	@Test
+	void testDeleteTagGroupData() {
+		
+		assert GenericTagGroupProperties.findAllByUserIdAndTagGroupId(userId, tagGroupInstance.id).size() == 1
+		
+		controller.session.userId = userId
+		controller.params["id"] = tagGroupInstance.id
+		
+		controller.deleteTagGroupData()
+		
+		assert controller.response.json.success == true
+		assert GenericTagGroupProperties.findAllByUserIdAndTagGroupId(userId, tagGroupInstance.id).size() == 0
+	}
+
+}
