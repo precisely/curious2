24781e68db6cc8edf1b71d357e97aa773035c42d
ujjawalgayakwad <ujjawal.gayakwad@causecode.com>
2015-07-23 18:32:12 +0530

    Reverting the changes to fix username problem in discussion list. #593

--- grails-app/services/us/wearecurio/services/SearchService.groovy
+++ grails-app/services/us/wearecurio/services/SearchService.groovy
@@ -162,7 +162,6 @@ class SearchService {
 					hash: d.hash,
 					name: d.name,
 					userId: d.userId,
-					userName: User.get(d.userId).username,
 					isPublic: d.isPublic(),
 					created: d.created,
 					updated: d.updated,
