3cafae676cd14c6286bb672d4135845b3f2c48d4
Mitsu Hadeishi <mitsu@syntheticzero.com>
2015-08-05 03:10:59 -0400

    Re #608 fixes for installbackup and installdb scripts --- tested and verified works now, support multiple db's on the same server

--- deploymentscripts/installdb/curiousinit_template.sql
+++ deploymentscripts/installdb/curiousinit_template.sql
@@ -1,5 +1,6 @@
 create user 'DBUSERNAME'@'WEBIP' identified by 'DBPASSWORD';
 create user 'DBUSERNAME'@'ANALYTICSIP' identified by 'DBPASSWORD';
+create user 'DBUSERNAME'@'localhost' identified by 'DBPASSWORD';
 
 create database DBNAME;
 create database DBNAME_dev;
@@ -11,5 +12,8 @@ grant all on DBNAME_test.* to 'DBUSERNAME'@'WEBIP';
 grant all on DBNAME.* to 'DBUSERNAME'@'ANALYTICSIP';
 grant all on DBNAME_dev.* to 'DBUSERNAME'@'ANALYTICSIP';
 grant all on DBNAME_test.* to 'DBUSERNAME'@'ANALYTICSIP';
+grant all on DBNAME.* to 'DBUSERNAME'@'localhost';
+grant all on DBNAME_dev.* to 'DBUSERNAME'@'localhost';
+grant all on DBNAME_test.* to 'DBUSERNAME'@'localhost';
 
 flush privileges;
