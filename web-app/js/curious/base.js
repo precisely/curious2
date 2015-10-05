a569688509d36d069f0d680fc78f5f90cefbd8eb
ujjawalgayakwad <ujjawal.gayakwad@causecode.com>
2015-10-05 16:05:39 +0530

    UI fixes. #641

--- web-app/js/curious/base.js
+++ web-app/js/curious/base.js
@@ -560,3 +560,7 @@ function dataURItoBlob(dataURI) {
 	}
 	return new Blob([ia], {type:mimeString});
 }
+
+String.prototype.capitalizeFirstLetter = function() {
+	return this.charAt(0).toUpperCase() + this.slice(1);
+}
