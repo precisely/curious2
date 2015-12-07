75a26e15704f3049103edaf9741c1ef4aead653a
Rich Devine <rich@wearecurio.us>
2015-12-07 11:25:58 -0500

    fixed merge issues

--- test/integration/us/wearecurio/search/integration/SearchSprintsIntegrationSpec.groovy
+++ test/integration/us/wearecurio/search/integration/SearchSprintsIntegrationSpec.groovy
@@ -53,6 +53,27 @@ class SearchSprintsIntegrationSpec extends SearchServiceIntegrationSpecBase {
 	}
 	
 	//@spock.lang.IgnoreRest
+	void "Test search for sprint with wildcard"() {
+		given: "a search term"
+		def searchTerm = "magnesium"
+		
+		and: "a new public sprint"
+		Sprint s = Sprint.create(new Date(), user2, "sprPublicRecentNameMatch $uniqueName $searchTerm", Visibility.PUBLIC)
+				
+		when: "elasticsearch service is indexed"
+		elasticSearchService.index()
+		elasticSearchAdminService.refresh("us.wearecurio.model_v0")
+
+		and: "search is performed"
+		def results = searchService.search(user1, "mag*")
+		
+		then: "matched sprint is found"
+		results.success
+		results.listItems.size == 1
+		results.listItems[0].hash == s.hash
+	}
+	
+	//@spock.lang.IgnoreRest
 	void "Test search for sprint with partial word"() {
 		given: "a search term"
 		def searchTerm = "magnesium"
