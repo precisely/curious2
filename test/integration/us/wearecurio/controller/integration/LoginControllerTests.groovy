d8a4340031f5c1f98a14a32f6b04860c0f27a979
Rich Devine <rich@wearecurio.us>
2016-03-29 13:31:36 +0900

    fixed register test to account for fact that it now returns parameters

--- test/integration/us/wearecurio/controller/integration/LoginControllerTests.groovy
+++ test/integration/us/wearecurio/controller/integration/LoginControllerTests.groovy
@@ -307,7 +307,7 @@ public class LoginControllerTests extends CuriousControllerTestCase {
 		assert view.equals("/login/register")
 	}
 
-	//@Test
+	@Test
 	void testDoregisterSuccess() {
 		LoginController controller = new LoginController()
 		
@@ -341,7 +341,7 @@ public class LoginControllerTests extends CuriousControllerTestCase {
 		
 		def rU = controller.response.redirectedUrl
 		
-		assert rU.endsWith('/home/index')
+		assert rU.contains('/home/index?')
 	}
 
 	@Test
