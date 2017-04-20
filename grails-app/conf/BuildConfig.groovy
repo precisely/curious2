fe33221dc4effa13198f9673e09f27fd7c9f392d
Vishesh Duggar <vishesh@causecode.com>
2017-04-20 15:59:25 +0530

    Removing the dev-checks plugin for now

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -104,10 +104,6 @@ grails.project.dependency.resolution = {
         //compile ":less-asset-pipeline:1.10.0"
         //compile ":coffee-asset-pipeline:1.8.0"
         //compile ":handlebars-asset-pipeline:1.3.0.3"
-
-        if (Environment.current == Environment.DEVELOPMENT) {
-            compile "com.causecode.plugins:dev-checks:1.0.0"
-        }
     }
 }
 
