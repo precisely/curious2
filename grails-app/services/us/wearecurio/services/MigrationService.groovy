3fc898fde6beefed5372465f6da310f6f7ddebfa
Ankit Agrawal <ankit.agrawal@causecode.com>
2017-06-23 17:36:25 +0530

    Updated migration tag

--- grails-app/services/us/wearecurio/services/MigrationService.groovy
+++ grails-app/services/us/wearecurio/services/MigrationService.groovy
@@ -1087,7 +1087,7 @@ class MigrationService {
 
 // - - - - - - - - - - - - - - - - Migrations to add admin to System group - - - - - - - - - - - - - - - - - - - - - - -
 
-		tryMigration("Add admin to System group") {
+		tryMigration("Add visheshd as admin to System group") {
 			UserGroup.lookup(UserGroup.SYSTEM_USER_GROUP_NAME).addAdmin(User.findByUsername("visheshd"))
 		}
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
